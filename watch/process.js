b3882a060267dc0f84566e361272349adddb083d
Meng Wang <water.wang@live.com>
2017-04-01 21:01:34 -0500

    watch

--- watch/process.js
+++ watch/process.js
@@ -22,9 +22,10 @@ err = null
 stdout = 
 stderr = 
 */
-exec(cmd, function (err, stdout, stderr) {
-    console.log(stdout.split('\n'));
-});
+// exec(cmd, function (err, stdout, stderr) {
+
+//     console.log(stdout.split('\n').filter());
+// });
 // console.log(process.platform);
 
 // if (true == true && parseInt('dsada')) {
@@ -32,3 +33,22 @@ exec(cmd, function (err, stdout, stderr) {
 // } else {
 //     console.log(false);
 // }
+setInterval(function () {
+    exec(cmd, function (err, stdout, stderr) {
+        if (err) {
+            console.log(err);
+        }
+        const re = (stdout.split('\n').filter(function (line) {
+            return line.trim().split(/\s+/)[0].toLowerCase() == 'leagueclient.exe';
+        }));
+        if (re.length > 0) {
+            for (let i in re) {
+                const p = re[i].trim().split(/\s+/);
+                console.log(p[0], p[1]);
+            }
+        } else {
+            console.log('notfound');
+        }
+        console.log(process.memoryUsage());
+    });
+}, 100);
