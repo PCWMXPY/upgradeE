63d15b494ca72dc0535cc73120b5f84a6b952e6f
Meng Wang <water.wang@live.com>
2017-04-17 20:06:55 -0500

    analysis

---
+++ pages/make/test.js
@@ -0,0 +1,4 @@
+re = [0, 0, 0, 0, 0];
+rere = 'swtr';
+rere += '[' + re + ']';
+console.log(typeof 0 == 'string');
--- src/preference.js
+++ src/preference.js
@@ -1,5 +1,5 @@
 var preference = {
-    getChampDeatil: function(Id) {
+    getChampDeatil: function (Id) {
         switch (Id) {
             //bf, tuan zhan, kongzhi, he li xing
             case 1:
@@ -272,7 +272,280 @@ var preference = {
                 return [0, 0, 0, 0, 0, 0];
         }
     },
-    getChampName: function(Id) {
+    getlane: id => {
+        //mid,bot,top,sup,jungle
+        switch (Id) {
+            case 1:
+                return [1, 5, 3, 2, 4];
+            case 2:
+                return [4, 5, 1, 3, 2];
+            case 3:
+                return [1, 5, 3, 2, 4];
+            case 4:
+                return "TwistedFate";
+            case 5:
+                return "XinZhao";
+            case 6:
+                return "Urgot";
+            case 7:
+                return "Leblanc";
+            case 8:
+                return "Vladimir";
+            case 9:
+                return "FiddleSticks";
+            case 10:
+                return "Kayle";
+            case 11:
+                return "MasterYi";
+            case 12:
+                return "Alistar";
+            case 13:
+                return "Ryze";
+            case 14:
+                return "Sion";
+            case 15:
+                return "Sivir";
+            case 16:
+                return "Soraka";
+            case 17:
+                return "Teemo";
+            case 18:
+                return "Tristana";
+            case 19:
+                return "Warwick";
+            case 20:
+                return "Nunu";
+            case 21:
+                return "MissFortune";
+            case 22:
+                return "Ashe";
+            case 23:
+                return "Tryndamere";
+            case 24:
+                return "Jax";
+            case 25:
+                return "Morgana";
+            case 26:
+                return "Zilean";
+            case 27:
+                return "Singed";
+            case 28:
+                return "Evelynn";
+            case 29:
+                return "Twitch";
+            case 30:
+                return "Karthus";
+            case 31:
+                return "Chogath";
+            case 32:
+                return "Amumu";
+            case 33:
+                return "Rammus";
+            case 34:
+                return "Anivia";
+            case 35:
+                return "Shaco";
+            case 36:
+                return "DrMundo";
+            case 37:
+                return "Sona";
+            case 38:
+                return "Kassadin";
+            case 39:
+                return "Irelia";
+            case 40:
+                return "Janna";
+            case 41:
+                return "Gangplank";
+            case 42:
+                return "Corki";
+            case 43:
+                return "Karma";
+            case 44:
+                return "Taric";
+            case 45:
+                return "Veigar";
+            case 48:
+                return "Trundle";
+            case 50:
+                return "Swain";
+            case 51:
+                return "Caitlyn";
+            case 53:
+                return "Blitzcrank";
+            case 54:
+                return "Malphite";
+            case 55:
+                return "Katarina";
+            case 56:
+                return "Nocturne";
+            case 57:
+                return "Maokai";
+            case 58:
+                return "Renekton";
+            case 59:
+                return "JarvanIV";
+            case 60:
+                return "Elise";
+            case 61:
+                return "Orianna";
+            case 62:
+                return "MonkeyKing";
+            case 63:
+                return "Brand";
+            case 64:
+                return "LeeSin";
+            case 67:
+                return "Vayne";
+            case 68:
+                return "Rumble";
+            case 69:
+                return "Cassiopeia";
+            case 72:
+                return "Skarner";
+            case 74:
+                return "Heimerdinger";
+            case 75:
+                return "Nasus";
+            case 76:
+                return "Nidalee";
+            case 77:
+                return "Udyr";
+            case 78:
+                return "Poppy";
+            case 79:
+                return "Gragas";
+            case 80:
+                return "Pantheon";
+            case 81:
+                return "Ezreal";
+            case 82:
+                return "Mordekaiser";
+            case 83:
+                return "Yorick";
+            case 84:
+                return "Akali";
+            case 85:
+                return "Kennen";
+            case 86:
+                return "Garen";
+            case 89:
+                return "Leona";
+            case 90:
+                return "Malzahar";
+            case 91:
+                return "Talon";
+            case 92:
+                return "Riven";
+            case 96:
+                return "KogMaw";
+            case 98:
+                return "Shen";
+            case 99:
+                return "Lux";
+            case 101:
+                return "Xerath";
+            case 102:
+                return "Shyvana";
+            case 103:
+                return "Ahri";
+            case 104:
+                return "Graves";
+            case 105:
+                return "Fizz";
+            case 106:
+                return "Volibear";
+            case 107:
+                return "Rengar";
+            case 110:
+                return "Varus";
+            case 111:
+                return "Nautilus";
+            case 112:
+                return "Viktor";
+            case 113:
+                return "Sejuani";
+            case 114:
+                return "Fiora";
+            case 115:
+                return "Ziggs";
+            case 117:
+                return "Lulu";
+            case 119:
+                return "Draven";
+            case 120:
+                return "Hecarim";
+            case 121:
+                return "Khazix";
+            case 122:
+                return "Darius";
+            case 126:
+                return "Jayce";
+            case 131:
+                return "Diana";
+            case 133:
+                return "Quinn";
+            case 134:
+                return "Syndra";
+            case 136:
+                return "AurelionSol";
+            case 143:
+                return "Zyra";
+            case 150:
+                return "Gnar";
+            case 154:
+                return "Zac";
+            case 161:
+                return "Velkoz";
+            case 163:
+                return "Taliyah";
+            case 427:
+                return "Ivern";
+            case 202:
+                return "Jhin";
+            case 240:
+                return "Kled";
+            case 267:
+                return "Nami";
+            case 201:
+                return "Braum";
+            case 203:
+                return "Kindred";
+            case 236:
+                return "Shen";
+            case 238:
+                return "Zed";
+            case 268:
+                return "Azir";
+            case 222:
+                return "Jinx";
+            case 420:
+                return "Illaoi";
+            case 223:
+                return "TahmKench";
+            case 432:
+                return "Bard";
+            case 412:
+                return "Thresh";
+            case 421:
+                return "RekSai";
+            case 254:
+                return "Vi";
+            case 245:
+                return "Ekko";
+            case 157:
+                return "Yasuo";
+            case 127:
+                return "Lissandra";
+            case 429:
+                return "Kalista";
+            case 266:
+                return "Aatrox";
+            default:
+                return Id;
+        }
+    },
+    getChampName: function (Id) {
         switch (Id) {
             case 1:
                 return "Annie";
@@ -544,7 +817,7 @@ var preference = {
                 return Id;
         }
     },
-    getSpellName: function(id) {
+    getSpellName: function (id) {
         switch (id) {
             case 21:
                 return "Barrier";
@@ -577,3 +850,4 @@ var preference = {
         }
     }
 };
+// module.exports = preference;
