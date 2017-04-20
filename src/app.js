7450a95eb06121aa034b434bea1ef8f188da7645
WMXPY <wm@wmpcxpy.com>
2017-04-20 12:24:03 -0500

    bug fix

--- src/app.js
+++ src/app.js
@@ -132,6 +132,8 @@ function createWindow() {
     mainWindow.openDevTools();
     // 监听浏览器窗口对象是否关闭，关闭之后直接将mainWindow指向空引用，也就是回收对象内存空间
     mainWindow.on("closed", function () {
+        currentsession.miao = null;
+        renders.mainpage = null;
         mainWindow = null;
     });
 }
